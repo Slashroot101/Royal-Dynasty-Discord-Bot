8f45ca0a353c53e88b5ca6a290cb731f316b2d68
Samuel Curry <samc735@gmail.com>
2023-11-22 20:12:23 -0500

    some schema tweeks

--- src/libs/database/schemas.ts
+++ src/libs/database/schemas.ts
@@ -19,6 +19,11 @@ export const DiscordGuildMemberSchema = schema.table("discord_guild_member", {
     updatedAt: timestamp("updatedAt", {mode: 'date'}).defaultNow().notNull()
 })
 
+export const discordGuildMemberRelation = relations(DiscordGuildMemberSchema, ({many, one}) => ({
+    guildMemberToUserGuildBank: many(userGuildBankSchema),
+    guildMembeerToUser: one(DiscordUserSchema)
+}))
+
 export const DiscordGuildSchema = schema.table("discord_guild", {
     id: uuid("id").notNull().primaryKey(),
     snowflake: text("snowflake").notNull(),
@@ -27,7 +32,8 @@ export const DiscordGuildSchema = schema.table("discord_guild", {
 })
 
 export const discordUserGuildMemberRelation = relations(DiscordUserSchema, ({many}) => ({
-    userToGuildMember: many(DiscordGuildMemberSchema)
+    userToGuildMember: many(DiscordGuildMemberSchema),
+    commandExecution: many(commandExecutionSchema)
 }))
 
 export const commandSchema = schema.table("command", {
@@ -40,6 +46,10 @@ export const commandSchema = schema.table("command", {
     updatedAt: timestamp("updatedAt", {mode: 'date'}).defaultNow().notNull()
 })
 
+export const commandRelations = relations(commandSchema, ({many}) => ({
+    commandToGuildCommand: many(guildCommandSchema)
+}))
+
 export const guildCommandSchema = schema.table("guild_command", {
     id: uuid("id").notNull().primaryKey(),
     commandId: uuid("commandId").notNull(),
@@ -50,6 +60,12 @@ export const guildCommandSchema = schema.table("guild_command", {
     updatedAt: timestamp("updatedAt", {mode: 'date'}).defaultNow().notNull()
 })
 
+export const guildCommandRelations = relations(guildCommandSchema, ({one}) => ({
+    guildCommandToCommand: one(commandSchema),
+    discordGuild: one(DiscordGuildSchema)
+}))
+
+
 export const commandExecutionSchema = schema.table("command_execution", {
     id: uuid("id").notNull().primaryKey(),
     commandId: uuid("commandId").notNull(),
@@ -57,3 +73,33 @@ export const commandExecutionSchema = schema.table("command_execution", {
     createdAt: timestamp("createdAt", {mode: 'date'}).defaultNow().notNull(),
     updatedAt: timestamp("updatedAt", {mode: 'date'}).defaultNow().notNull()
 })
+
+export const commandExecutionRelations = relations(commandExecutionSchema, ({many, one}) => ({
+    discordGuildMember: one(DiscordGuildMemberSchema),
+    commandId: one(commandSchema)
+}))
+
+export const userGuildBankSchema = schema.table("user_guild_bank", {
+    id: uuid("id").notNull().primaryKey(),
+    discordGuildMemberId: uuid("discordGuildId").notNull(),
+    balance: integer("balance").notNull().default(0),
+    createdAt: timestamp("createdAt", {mode: 'date'}).defaultNow().notNull(),
+    updatedAt: timestamp("updatedAt", {mode: 'date'}).defaultNow().notNull()
+})
+
+export const userGuildBankTransactionUserGuildBankRelation = relations(userGuildBankSchema, ({many}) => ({
+    userGuildBankToUserGuildBankTransaction: many(userGuildBankTransactionSchema),
+    discordGuildMember: many(DiscordGuildMemberSchema),
+}))
+
+export const userGuildBankTransactionSchema = schema.table("user_guild_bank_transaction", {
+    id: uuid("id").notNull().primaryKey(),
+    userGuildBankId: uuid("userGuildBankId").notNull(),
+    amount: integer("amount").notNull(),
+    createdAt: timestamp("createdAt", {mode: 'date'}).defaultNow().notNull(),
+    updatedAt: timestamp("updatedAt", {mode: 'date'}).defaultNow().notNull()
+})
+
+export const transactionToUserGuildBank = relations(userGuildBankTransactionSchema, ({one}) => ({
+    userGuildBank: one(userGuildBankSchema)
+}))
